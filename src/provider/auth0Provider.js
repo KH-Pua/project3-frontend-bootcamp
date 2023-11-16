553b107eaa52d362ffa3266bea60c65f4c7e2355
spenceryang <yangzijian@gmail.com>
2023-11-16 12:28:11 +0900

    redirect to netlify app

--- src/provider/auth0Provider.js
+++ src/provider/auth0Provider.js
@@ -7,9 +7,10 @@ export default function OAuthProvider({ children }) {
       domain="dev-2jf01eugzd36x5ye.us.auth0.com"
       clientId="FwWeA5Y07sVMwlDX7Q97oClY8KTrFTLa"
       authorizationParams={{
-        redirect_uri: "http://localhost:3000/listingAll",
-        scope: "read:current_user update:current_user_metadata openid profile email",
-        audience: "https://api.powderful.xyz" // FE audience's URL must tally with BE audience's URL
+        redirect_uri: "http://powderful.netlify.app/listingAll",
+        scope:
+          "read:current_user update:current_user_metadata openid profile email",
+        audience: "https://api.powderful.xyz", // FE audience's URL must tally with BE audience's URL
       }}
     >
       {children}
